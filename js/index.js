15553b3d5dfdf2699472398da38d191aaf94bb47
[LowlyYl] <[artsiushko.yl@mail.ru]>
2021-08-29 15:32:41 +0300

    weather

---
+++ js/index.js
@@ -0,0 +1,125 @@
+import { WeatherService } from "./WeatherService.js";
+
+let temp = document.getElementById("condition");
+let city = document.getElementById("city");
+let description = document.getElementById("description");
+let icon = document.getElementById("icon");
+let wind = document.getElementById("wind");
+let humidity = document.getElementById("humidity");
+let fahrenheit = document.getElementById("fahrenheit");
+let celsius = document.getElementById("celsius");
+let form = document.querySelector("form");
+let panel = document.getElementById("panel");
+let preloader = document.getElementById("preloader");
+let dt = document.getElementById("dt");
+let forecast = document.getElementById("forecast");
+
+forecast.style = `
+  display: flex;
+  justify-content: space-around;
+`;
+
+let cityText = "Minsk";
+let units = "imperial";
+
+fahrenheit.onclick = () => {
+  fahrenheit.classList.add("active");
+  celsius.classList.remove("active");
+
+  units = "imperial";
+  WeatherService.getWeatherByText(cityText, units).then((result) =>
+    renderWeatherData(result)
+  );
+};
+
+celsius.onclick = () => {
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+
+  units = "metric";
+  WeatherService.getWeatherByText(cityText, units).then((result) =>
+    renderWeatherData(result)
+  );
+};
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+  let input = event.target[0];
+
+  WeatherService.getWeatherByText(input.value, units)
+    .then((result) => renderWeatherData(result))
+    .catch(() => {
+      input.value = "";
+
+      alert("entry correct data");
+    });
+};
+
+WeatherService.getWeatherByText(cityText).then((result) =>
+  renderWeatherData(result)
+);
+
+function renderWeatherData(weatherData) {
+  if (weatherData?.name) {
+    panel.classList.remove("transparent");
+    preloader.classList.remove("active");
+  }
+
+  temp.innerHTML = weatherData.main.temp;
+  city.innerHTML = weatherData.name;
+  description.innerHTML = weatherData.weather[0].description;
+
+  let windText = `${weatherData.wind.speed} ${
+    units === "metric" ? "km/h" : "mph"
+  }`;
+
+  wind.innerHTML = windText;
+
+  let humidityText = `${weatherData.main.humidity}%`;
+
+  humidity.innerHTML = humidityText;
+  icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+
+  showDate();
+
+  WeatherService.getForecast(weatherData.name, units).then((result) => {
+    let { list } = result;
+    console.log(list)
+
+    let elemArr = list.map((item) => {
+      return ForecastItem({ maxTemp: item.temp.max, minTemp: item.temp.min, sunrise: item.sunrise, sunset: item.sunset, date: item.dt });
+    });
+
+    forecast.innerHTML = "";
+    forecast.append(...elemArr);
+  });
+}
+
+function showDate() {
+  let date = new Date();
+
+  dt.innerHTML = date.toLocaleDateString();
+}
+
+function ForecastItem({ maxTemp, minTemp, date }) {
+  let div = document.createElement("div");
+
+  let today = new Date(date * 1000).toLocaleDateString();
+
+  div.style = `
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 33px;
+  `;
+
+  div.innerHTML = `
+  <p>${today}</p>
+  <p><label>Max</label> ${maxTemp}</p>  
+  <p><label>Min</label> ${minTemp}</p>
+  `;
+
+  return div;
+}
